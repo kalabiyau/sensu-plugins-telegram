1c62f8a821ebf905222b7f0d7f62bd207faab96d
Hernan Schmidt <lagartoflojo@users.noreply.github.com>
2016-03-22 17:00:18 +0100

    Fix rubocop offenses

--- bin/handler-telegram.rb
+++ bin/handler-telegram.rb
@@ -41,51 +41,50 @@ require 'restclient'
 require 'cgi'
 
 class TelegramHandler < Sensu::Handler
-
   def chat_id
-    @event["chat_id"] || settings["telegram"]["chat_id"]
+    @event['chat_id'] || settings['telegram']['chat_id']
   end
 
   def bot_token
-    @event["bot_token"] || settings["telegram"]["bot_token"]
+    @event['bot_token'] || settings['telegram']['bot_token']
   end
 
   def error_file
-    @event["error_file_location"] || settings["telegram"]["error_file_location"]
+    @event['error_file_location'] || settings['telegram']['error_file_location']
   end
 
   def event_name
-    client_name + "/" + check_name
+    client_name + '/' + check_name
   end
 
   def action_name
     actions = {
-      "create" => "Created",
-      "resolve" => "Resolved",
-      "flapping" => "Flapping"
+      'create' => 'Created',
+      'resolve' => 'Resolved',
+      'flapping' => 'Flapping'
     }
-    actions[@event["action"]]
+    actions[@event['action']]
   end
 
   def action_icon
     icons = {
-      "create" => "\xF0\x9F\x98\xB1",
-      "resolve" => "\xF0\x9F\x98\x8D",
-      "flapping" => "\xF0\x9F\x90\x9D"
+      'create' => "\xF0\x9F\x98\xB1",
+      'resolve' => "\xF0\x9F\x98\x8D",
+      'flapping' => "\xF0\x9F\x90\x9D"
     }
-    icons[@event["action"]]
+    icons[@event['action']]
   end
 
   def client_name
-    escape_html @event["client"]["name"]
+    escape_html @event['client']['name']
   end
 
   def check_name
-    escape_html @event["check"]["name"]
+    escape_html @event['check']['name']
   end
 
   def output
-    escape_html @event["check"]["output"]
+    escape_html @event['check']['output']
   end
 
   def escape_html(string)
@@ -110,16 +109,16 @@ class TelegramHandler < Sensu::Handler
     {
       chat_id: chat_id,
       text: build_message,
-      parse_mode: "HTML",
+      parse_mode: 'HTML',
       disable_web_page_preview: true
     }
   end
 
   def handle_error(exception)
     open(error_file, 'w') do |f|
-      f.puts "URL: " + telegram_url
-      f.puts "Params: " + params.inspect
-      f.puts "Exception: " + exception.inspect
+      f.puts 'URL: ' + telegram_url
+      f.puts 'Params: ' + params.inspect
+      f.puts 'Exception: ' + exception.inspect
     end if error_file
   end
 
@@ -128,15 +127,13 @@ class TelegramHandler < Sensu::Handler
   end
 
   def handle
-    begin
-      print "Sending to Telegram: " + params.inspect
-      RestClient.post(telegram_url, params, format: :json)
-      puts "... done."
-      clear_error
-    rescue SocketError, RestClient::Exception => e
-      handle_error(e)
-      puts "... Telegram handler error '#{e.inspect}' while attempting to report an incident: #{event_name} #{action_name}"
-    end
+    print 'Sending to Telegram: ' + params.inspect
+    RestClient.post(telegram_url, params, format: :json)
+    puts '... done.'
+    clear_error
+  rescue SocketError, RestClient::Exception => e
+    handle_error(e)
+    puts "... Telegram handler error '#{e.inspect}' while attempting to report an incident: #{event_name} #{action_name}"
   end
 
   def check_status
@@ -145,10 +142,10 @@ class TelegramHandler < Sensu::Handler
 
   def translate_status
     status = {
-      0 => "OK",
-      1 => "Warning",
-      2 => "Critical",
-      3 => "Unknown"
+      0 => 'OK',
+      1 => 'Warning',
+      2 => 'Critical',
+      3 => 'Unknown'
     }
     status[check_status.to_i]
   end
